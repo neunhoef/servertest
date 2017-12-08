ee0bd5fc917a4386b1c116d155bfeddf063f8231
Max Neunhoeffer <max@arangodb.com>
2017-12-08 21:25:11 +0100

    More progress.

--- servertest.cpp
+++ servertest.cpp
@@ -88,28 +88,30 @@ void multipleThreads(Work* work, std::mutex* mutex, std::atomic<int>* stop,
 class Server {
  public:
   struct alignas(128) Client {
-    std::atomic<int32_t> what; // starts as 0, which means nothing to do
-                               // a positive number indicates that the client
-                               // wants a job to be done, when done, the server
-                               // sets it to the negative of the job id
+    std::atomic<uint32_t> inTick;  // starts as 0, an increase means that
+                                   // a new job has to be done
+    uint32_t what;    // indicates what to do
     Work* work;
-    char padding[122];
-    Client(Work* w) : what(0), work(w) {
-    }
+    char padding[120 - sizeof(Work*)];
+    std::atomic<uint32_t> outTick;  // starts as 0, an increase means that
+                                    // a new answer is there
+    char padding2[124];
+    Client(Work* w) : inTick(0), what(0), work(w), outTick(0) { }
   };
 
  private:
   std::mutex mutex;
   size_t maxNrClients;
   Client** clients;
+  uint32_t* inTicks;
   std::atomic<size_t> nrClients;
   bool started;
   std::thread server;
 
  public:
   Server(size_t m) 
-    : maxNrClients(m), clients(new Client*[m]), nrClients(0), started(false),
-      server(&Server::run, this) {
+    : maxNrClients(m), clients(new Client*[m]), inTicks(new uint32_t[m]),
+      nrClients(0), started(false), server(&Server::run, this) {
   }
 
   ~Server() {
@@ -117,6 +119,7 @@ class Server {
       std::cout << "Warning: Server has clients on destruction!" << std::endl;
     }
     server.join();
+    delete[] inTicks;
     delete[] clients;
   }
 
@@ -126,6 +129,7 @@ class Server {
       return false;
     }
     clients[nrClients] = c;
+    inTicks[nrClients] = 0;
     ++nrClients;
     started = true;
     return true;
@@ -143,6 +147,7 @@ class Server {
       return;
     }
     clients[i] = clients[nrClients-1];
+    inTicks[i] = inTicks[nrClients-1];
     --nrClients;
   }
 
@@ -153,10 +158,77 @@ class Server {
       switch (nrClients) {
         case 1:
           for (size_t j = 0; j < 100; ++j) {
-            int32_t what = clients[0]->what.load(std::memory_order_acquire);
-            if (what > 0) {
+            uint32_t t = clients[0]->inTick.load(std::memory_order_acquire);
+            if (t != inTicks[0]) {
+              inTicks[0] = t;
               clients[0]->work->dowork();
-              clients[0]->what.store(-what, std::memory_order_release);
+              clients[0]->outTick.store(t, std::memory_order_release);
+            }
+          }
+          break;
+        case 2:
+          for (size_t j = 0; j < 100; ++j) {
+            uint32_t t = clients[0]->inTick.load(std::memory_order_acquire);
+            if (t != inTicks[0]) {
+              inTicks[0] = t;
+              clients[0]->work->dowork();
+              clients[0]->outTick.store(t, std::memory_order_release);
+            }
+            t = clients[1]->inTick.load(std::memory_order_acquire);
+            if (t != inTicks[1]) {
+              inTicks[1] = t;
+              clients[1]->work->dowork();
+              clients[1]->outTick.store(t, std::memory_order_release);
+            }
+          }
+          break;
+        case 3:
+          for (size_t j = 0; j < 100; ++j) {
+            uint32_t t = clients[0]->inTick.load(std::memory_order_acquire);
+            if (t != inTicks[0]) {
+              inTicks[0] = t;
+              clients[0]->work->dowork();
+              clients[0]->outTick.store(t, std::memory_order_release);
+            }
+            t = clients[1]->inTick.load(std::memory_order_acquire);
+            if (t != inTicks[1]) {
+              inTicks[1] = t;
+              clients[1]->work->dowork();
+              clients[1]->outTick.store(t, std::memory_order_release);
+            }
+            t = clients[2]->inTick.load(std::memory_order_acquire);
+            if (t != inTicks[2]) {
+              inTicks[2] = t;
+              clients[2]->work->dowork();
+              clients[2]->outTick.store(t, std::memory_order_release);
+            }
+          }
+          break;
+        case 4:
+          for (size_t j = 0; j < 100; ++j) {
+            uint32_t t = clients[0]->inTick.load(std::memory_order_acquire);
+            if (t != inTicks[0]) {
+              inTicks[0] = t;
+              clients[0]->work->dowork();
+              clients[0]->outTick.store(t, std::memory_order_release);
+            }
+            t = clients[1]->inTick.load(std::memory_order_acquire);
+            if (t != inTicks[1]) {
+              inTicks[1] = t;
+              clients[1]->work->dowork();
+              clients[1]->outTick.store(t, std::memory_order_release);
+            }
+            t = clients[2]->inTick.load(std::memory_order_acquire);
+            if (t != inTicks[2]) {
+              inTicks[2] = t;
+              clients[2]->work->dowork();
+              clients[2]->outTick.store(t, std::memory_order_release);
+            }
+            t = clients[3]->inTick.load(std::memory_order_acquire);
+            if (t != inTicks[3]) {
+              inTicks[3] = t;
+              clients[3]->work->dowork();
+              clients[3]->outTick.store(t, std::memory_order_release);
             }
           }
           break;
@@ -176,10 +248,11 @@ void clientThread(Server* server, Work* work, std::atomic<int>* stop,
   // simply work as client until stop is signalled:
   uint64_t c = 0;
   size_t perRound = ceill(1e-5 / workTime);
+  uint32_t t = 0;
   while (stop->load() == 0) {
     for (size_t i = 0; i < perRound; ++i) {
-      cl->what.store(2, std::memory_order_release);
-      while (cl->what.load(std::memory_order_acquire) != -2) {
+      cl->inTick.store(++t, std::memory_order_release);
+      while (cl->outTick.load(std::memory_order_acquire) != t) {
       }
       ++c;
     }
@@ -295,22 +368,43 @@ int main(int argc, char* argv[]) {
   // Measure a delegating server:
   {
     std::cout << "Running in a single thread with delegation..." << std::endl;
-    Server server(threads);  // start the server thread
-    std::atomic<int> stop(0);
-    uint64_t count;
-    auto startTime = clock.now();
-    std::thread t(clientThread, &server, &work, &stop, &count);
-    std::this_thread::sleep_for(std::chrono::duration<double>(testTime));
-    stop.store(1);
-    t.join();
-    auto endTime = clock.now();
-    std::chrono::duration<double> runTime = endTime - startTime;
-    std::cout << "  time="
-      << runTime.count() << "s " << pretty(count)
-      << " iterations, time per iteration: "
-      << floorl(runTime.count() / static_cast<double>(count) * 1e9) << " ns"
-      << std::endl;
-    
+    for (int j = 1; j <= threads; ++j) {
+      Server server(threads);  // start the server thread
+      std::cout << "Using " << j << " threads:" << std::endl;
+      std::atomic<int> stop(0);
+      std::vector<std::thread> ts;
+      std::vector<uint64_t> counts;
+      counts.reserve(j);
+      for (int i = 0; i < j; ++i) {
+        counts.push_back(0);
+      }
+      ts.reserve(j);
+      auto startTime = clock.now();
+      for (int i = 0; i < j; ++i) {
+        ts.emplace_back(clientThread, &server, &work, &stop, &counts[i]);
+      }
+      std::this_thread::sleep_for(std::chrono::duration<double>(testTime));
+      stop.store(1);
+      for (int i = 0; i < j; ++i) {
+        ts[i].join();
+      }
+      auto endTime = clock.now();
+      std::chrono::duration<double> runTime = endTime - startTime;
+      uint64_t count = 0;
+      for (int i = 0; i < j; ++i) {
+        count += counts[i];
+      }
+      std::cout << "  time="
+        << runTime.count() << "s " << pretty(count)
+        << " iterations, time per iteration: "
+        << floorl(runTime.count() / static_cast<double>(count) * 1e9) << " ns"
+        << std::endl;
+      std::cout << "  thread counts:";
+      for (int i = 0; i < j; ++i) {
+        std::cout << " " << pretty(counts[i]);
+      }
+      std::cout << "\n" << std::endl;
+    }
   }
 
   // Write out dummy result to convince compiler not to optimize everything out
